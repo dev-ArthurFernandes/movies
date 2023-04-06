8d3e79d3c351afcac6bf93035667b69520078d74
Arthur <arthurdesosuafernandes@gmail.com>
2023-04-06 15:47:06 -0300

    feat: Conenct Database

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express, { Application, json } from 'express';
+import { startDataBase } from './database';
 
 
 const App: Application = express()
 
+const baseURL: string = "movies"
+
 App.use(json())
 
+App.get(`${baseURL}`)
+App.get(`${baseURL}/:id`)
+
+App.post(`${baseURL}`)
+
+App.patch(`${baseURL}/:id`)
+
+App.delete(`${baseURL}/:id`)
 
 
-
-App.listen(3000, () => {
+App.listen(3000, async () => {
+    await startDataBase()
     console.log("Server is running!")
 })
